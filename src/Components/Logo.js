be790f1141790c4dacf05a583ccbfce82fdfd245
Prashant Shrestha <intern0t@users.noreply.github.com>
2018-07-05 08:17:04 -0400

    Staging, for now

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Logo = () => {
-    return <img src="../../256horizontal.png" alt="Logo by dee-y" />;
+    return <img src="https://i.imgur.com/8Hjh62H.png" alt="Logo by dee-y" />;
 };
 
 export default Logo;
